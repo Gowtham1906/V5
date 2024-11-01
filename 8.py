d36318787f75ec8de35ad8e789e7f5804cc70ea4
S.GOWTHAM <88620210+Gowtham1906@users.noreply.github.com>
2024-11-01 10:19:35 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -709,7 +709,7 @@ def main():
         
         # Configuration
         region = "us-east-1"
-        vpc_ids = ["vpc-00ca93cd8e10052a6","vpc-008e46fde5ccc685a"]  # Add your new VPC IDs here  "vpc-008e46fde5ccc685a"
+        vpc_ids = ["vpc-055b2e670d7bfdace"]  # Add your new VPC IDs here  "vpc-008e46fde5ccc685a"
         
         # Create Terraform files only if they don't exist
         if not os.path.exists(parent_module) or not os.path.exists(child_module):
